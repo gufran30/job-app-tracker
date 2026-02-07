513f83b1a6896099e3d7868a208549de348374a2
Gufran Khan <gufran3004@gmail.com>
2026-02-07 17:53:47 +0530

    Integrate Better Auth

---
+++ lib/auth/auth-client.ts
@@ -0,0 +1,10 @@
+// This file is going to contain some functions that are useful for us
+// and we can use in client components
+
+import { createAuthClient } from "better-auth/react";
+
+export const authClient = createAuthClient({
+  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
+});
+
+export const { signIn, signUp, signOut, useSession } = authClient;
